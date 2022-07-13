dbfe2e28e27fa5c859722c71e199dc90f237f905
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-13 19:21:34 +0300

    content generated. last edition done. end..

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
-import React, { useState } from "react";
+import React  from "react";
+import Accordion from "./Accordion";
+import { accordionData } from "./utils/content";
 
 function App() {
-  const accordionData = {
-    title: "section 1",
-    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maxime itaque odit esse non vitae veritatis numquam quia amet obcaecati? Libero, eos necessitatibus hic sint iusto reiciendis tempore est doloribus.`,
-  };
+  
 
-  const { title, content } = accordionData;
-
-  const [isActive, setIsActive] = useState(false);
   return (
     <div className="App">
       <h1>Accordion Demo</h1>
       <div className="accordion">
-        <div className="accordion-item">
-          <div
-            className="accordion-title"
-            onClick={() => setIsActive(!isActive)}
-          >
-            <div>{title}</div>
-            <div>{isActive ? "-" : "+"}</div>
-          </div>
-          {isActive ? (
-            <div className="accordion-content">{content}</div>
-          ) : (
-            <div className="accordion-content">{content.slice(0, 30)}</div>
-          )}
-        </div>
+        {accordionData.map(({title,content})=>(
+          <Accordion title={title} content ={content}/>
+        ))}
       </div>
     </div>
   );
---
+++ src/utils/content.jsx
@@ -0,0 +1,14 @@
+export const accordionData = [
+  {
+    title: "section 1",
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maxime itaque odit esse non vitae veritatis numquam quia amet obcaecati? Libero, eos necessitatibus hic sint iusto reiciendis tempore est doloribus.`,
+  },
+  {
+    title: "section 2",
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maxime itaque odit esse non vitae veritatis numquam quia amet obcaecati? Libero, eos necessitatibus hic sint iusto reiciendis tempore est doloribus.`,
+  },
+  {
+    title: "section 3",
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maxime itaque odit esse non vitae veritatis numquam quia amet obcaecati? Libero, eos necessitatibus hic sint iusto reiciendis tempore est doloribus.`,
+  },
+];
